7407b59bbca7124d1a12381006ea2568c881eedb
jvernet <jeromevernet@hotmail.com>
2020-12-22 22:36:15 +0100

    Add XCode Project

---
+++ CAPSimage/CAPSimage.h
@@ -0,0 +1,18 @@
+//
+//  CAPSimage.h
+//  CAPSimage
+//
+//  Created by Jerome Vernet on 21/12/2020.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CAPSimage.
+FOUNDATION_EXPORT double CAPSimageVersionNumber;
+
+//! Project version string for CAPSimage.
+FOUNDATION_EXPORT const unsigned char CAPSimageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CAPSimage/PublicHeader.h>
+
+
